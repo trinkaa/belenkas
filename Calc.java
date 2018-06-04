52d361cb4f005f83fae880832fb7a6fecc87f924
trinkaa <tpranas@yahoo.com>
2018-06-04 22:59:29 +0300

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,16 @@
+package projectPackage;
+
+public class Calc {
+	int newVal;
+
+	/**
+	 * @param args
+	 */
+	Calc(){
+	}
+
+	public void myMethod(int k, int j) {
+		newVal = k + j;
+		System.out.println(newVal);
+	}
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+package projectPackage;
+
+/**
+ * @author Pranas
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		Calc newCalc = new Calc();
+		newCalc.myMethod(2, 5);
+	}
+}
